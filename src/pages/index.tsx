8d75cd4099f2b1b56f76f8e1b3da7420606c80b5
Gustavo Larsen <guslarsen@gmail.com>
2022-06-18 17:52:21 -0300

    configuracaoes de estrutura do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Podcastr</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+      </Head>
+
+      <main>Podcastr</main>
+    </div>
+  );
+}
